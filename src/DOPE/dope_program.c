2c5f621296604b3d601846ff06ff7cf1610480bc
Hellorld! <jeremy.thornton@nhs.net>
2025-08-19 14:01:52 +0100

    Update dope_program.c

--- src/DOPE/dope_program.c
+++ src/DOPE/dope_program.c
@@ -1,1 +1,22 @@
 #include "dope_program.h"
+
+dope_program_t* dope_new_program(size_t line_count) {
+    dope_program_t* program = malloc(sizeof(dope_data_sheet_t));
+    if (!program) {
+        return NULL;
+    }
+
+    program->tokens = calloc(line_count, sizeof(dope_token_t));
+    if (!program->tokens) {
+        free(tokens);
+        return NULL;
+    }
+
+    program->capacity = line_count;
+    program->size = 0;
+    return program;
+}
+
+void dope_free_program(dope_program_t* program) {
+
+}
