dbf262339fe1e51583341a9134ae582d1e3238dd
Hellorld! <jeremy.thornton@nhs.net>
2025-08-19 14:03:00 +0100

    Update dope_program.h

--- src/DOPE/dope_program.h
+++ src/DOPE/dope_program.h
@@ -18,18 +18,19 @@ static const uint8_t DOPE_FEILDS[] = {
 typedef struct {
     uint8_t line;
     uint8_t instr;
-    dope_data_field_t fields[DOPE_INSTRUCTION_FIELDS];
+    dope_data_field_t fields[DOPE_INSTRUCTION_FIELDS]; // 5
 } dope_token_t;
 
 typedef struct {
-    uint8_t ip;
+    int ip;
     dope_token_t tokens[DOPE_PROGRAM_LINES_MAX];
+    size_t size;
+    size_t capacity;
 } dope_program_t;
 
-dope_program_t* dope_new_program();
+dope_program_t* dope_new_program(size_t line_count);
 
 void dope_free_program(dope_program_t* program);
 
-void dope_tokenize_line(dope_line_t* line, dope_token_t* token);
 
 #endif
