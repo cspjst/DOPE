9efaab63e421203a4808f319f6a681cd77c8d54f
Hellorld! <jeremy.thornton@nhs.net>
2025-08-21 12:09:16 +0100

    Update dope_program.h

--- src/DOPE/dope_program.h
+++ src/DOPE/dope_program.h
@@ -9,16 +9,6 @@
 extern const char* const DOPE_INSTRUCTIONS[];
 extern const uint8_t DOPE_OPERAND_COUNT[];
 
-typedef enum {
-    DOPE_ERR_SUCCESS = 0,
-    DOPE_ERR_NO_INPUT,        // EOF
-    DOPE_ERR_NO_INSTR,        // empty line
-    DOPE_ERR_UNKNOWN_INSTR,   // invalid instruction
-    DOPE_ERR_TOO_FEW_ARGS,    // not enough operands
-    DOPE_ERR_TOO_MANY_ARGS,   // too many operands
-    DOPE_ERR_LINE_TOO_LONG    // input line too long
-} dope_error_t;
-
 typedef struct {
     uint8_t line_number;           
     uint8_t error_code;
