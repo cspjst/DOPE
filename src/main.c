ab4f2d71004dd091263c4b5ab4ab88af76c72df6
Hellorld! <jeremy.thornton@nhs.net>
2025-08-21 16:56:15 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,19 +1,8 @@
-#include "DOPE/dope_types.h"
-#include "DOPE/dope_sheet.h"
-#include "DOPE/dope_data_sheet.h"
+#include "DOPE/dope_program.h"
 
 int main() {
-    //dope_sheet_t* sheet = dope_new_sheet();
-    dope_data_sheet_t* data = dope_new_data_sheet(10);
-
-    //dope_input_sheet(sheet, stdin);
-    //dope_print_sheet(sheet);
-
-    dope_input_data_sheet(data, stdin);
-    dope_print_data_sheet(data);
-
-    //dope_free_sheet(sheet);
-    dope_free_data_sheet(data);
-
+    dope_program_t* prog = dope_new_program(10);
+    
+    dope_program_free(prog);
     return 0;
 }
