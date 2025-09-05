f68cade4dd1bef234ac3e3ef20d740dca1c512a7
Hellorld! <jeremy.thornton@nhs.net>
2025-09-05 10:40:21 +0100

    Update dope_constants.h

--- src/DOPE/dope_constants.h
+++ src/DOPE/dope_constants.h
@@ -7,7 +7,7 @@
 #define DOPE_PROGRAM_LINES_MAX     99  /**< Max number of source lines in program (1–99) */
 #define DOPE_LINE_SIZE             40  /**< Max length of input line including '\0' */
 #define DOPE_INSTRUCTION_PARTS      6  /**< Max tokens per line: 1 opcode + 5 operands */
-#define DOPE_FIELD_SIZE            10  /**< Max length of each operand string (e.g. "X", "13.5") */
+#define DOPE_FIELD_SIZE            12  /**< Max length of each operand string (e.g. "SQR", "-1111'11-11'") */
 #define DOPE_OPERANDS_MAX           5  /**< Max number of operands an instruction can take */
 #define DOPE_INSTRUCTION_COUNT     19  /**< Total number of valid opcodes in DOPE language */
 
