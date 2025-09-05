9f92caeb830a05aef925cf6f1f9255b47da59204
Hellorld! <jeremy.thornton@nhs.net>
2025-09-05 14:05:05 +0100

    Update dope_errors.c

--- src/DOPE/dope_errors.c
+++ src/DOPE/dope_errors.c
@@ -13,6 +13,7 @@ const char* const DOPE_ERROR_MESSAGES[] = {
     "Found a space at input!",  // DOPE_ERR_INVALID_CHAR
     "Wrong number format!",     // DOPE_ERR_INVALID_NUMBER_FORMAT
     "Exponent out of range!",   // DOPE_ERR_EXPONENT_OUT_OF_RANGE
+    "Missing stop code!",       // DOPE_ERR_MISSING_STOP_CODE
     "Absent/malformed FINISH!"  // DOPE_ERR_FINISH
 };
 
