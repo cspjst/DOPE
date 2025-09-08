2290af0536a7eb46a05ee9028227eaeee4578e32
Hellorld! <jeremy.thornton@nhs.net>
2025-09-08 14:25:12 +0100

    Create dope_vectors.c

---
+++ src/DOPE/dope_vectors.c
@@ -0,0 +1,15 @@
+#include "dope_vectors.h"
+
+dope_vectab_t* dope_new_vectab() { } // max 4 vectors
+
+void dope_free_vectab(dope_vectab_t* vectab){ }
+
+dope_vec_t* dope_find_vec(dope_vectab_t* vectab, const char name) { }
+
+dope_vec_t* dope_alloc_vec(dope_vectab_t* vectab, const char name) { }
+
+//void dope_dealloc_vec(dope_vectab_t* vectab, const char name) { } //no scope in DOPE so redundant
+
+void dope_set_val(dope_vectab_t* vectab, const char name, float value, dope_size_t i) { }
+
+float dope_get_val(const dope_vectab_t* vectab, const char name, dope_size_t i) { }
