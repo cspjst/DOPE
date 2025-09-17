3e61b4803df40b220b60c964c5271daf6af7498d
Hellorld! <jeremy.thornton@nhs.net>
2025-09-17 12:47:58 +0100

    Update parse_regex.h

--- src/DOPE/parse_regex.h
+++ src/DOPE/parse_regex.h
@@ -21,23 +21,24 @@
 typedef bool (*dope_regex_fn_t)(const char **rgx, const char **str, char expected);
 
 // sparse dispatch table - size 59 for chars from '$' - '$' to '^' - '$'
-static parse_regex_fn_t parse_regex_fn_call_table[59] = {0};
+static parse_regex_fn_t parse_regex_dispatch_table[59] = {0};
+static bool is_table_init = false;
 
-void parse_regex_init_call_table(); 
+void parse_regex_init_dispatch_table(); 
 
-bool parse_regex_match(const char **regex_ptr, const char **text_ptr)
+bool parse_regex_dispatch(const char **rgx, const char **str)
 
 // Main entry point: checks if the entire text matches the regex
 bool parse_regex(const char *regex, const char *text) {
-    return parse_regex_match(rgx, str);
+    return parse_regex_dispatch(rgx, str);
 }
 
-bool match_caret(const char **rgx, const char **str, char expected);
+bool parse_anchor_start(const char **rgx, const char **str, char expected);
 
-bool match_dollar(const char **rgx, const char **str, char expected);
+bool parse_anchor_end(const char **rgx, const char **str, char expected);
 
-bool match_dot(const char **rgx, const char **str, char expected);
+bool parse_wildcard(const char **rgx, const char **str, char expected);
 
-bool match_literal(const char **rgx, const char **str, char expected);
+bool parse_literal(const char **rgx, const char **str, char expected);
 
 #endif
